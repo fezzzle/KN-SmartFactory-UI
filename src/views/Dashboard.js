3ddab274be61d7dab46fab7eccfa15a34ae492bc
allikkil <killuallik@gmail.com>
2021-05-17 17:39:35 +0300

    changed chart headers

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -63,6 +63,68 @@ function Dashboard(props) {
     <>
       <div className="content">
         <Row>
+          <Card className="card-plain">
+            <Col sm="12">
+              <ButtonGroup
+                className="btn-group-toggle float-right"
+                data-toggle="buttons"
+              >
+                <Button
+                  tag="label"
+                  className={classNames("btn-simple", {
+                    active: bigChartData === "data1",
+                  })}
+                  color="info"
+                  id="0"
+                  size="sm"
+                  onClick={() => setBgChartData("data1")}
+                >
+                  <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                    Factories
+                        </span>
+                  <span className="d-block d-sm-none">
+                    <i className="tim-icons icon-single-02" />
+                  </span>
+                </Button>
+                <Button
+                  color="info"
+                  id="1"
+                  size="sm"
+                  tag="label"
+                  className={classNames("btn-simple", {
+                    active: bigChartData === "data2",
+                  })}
+                  onClick={() => setBgChartData("data2")}
+                >
+                  <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                    Production Lines
+                        </span>
+                  <span className="d-block d-sm-none">
+                    <i className="tim-icons icon-gift-2" />
+                  </span>
+                </Button>
+                <Button
+                  color="info"
+                  id="2"
+                  size="sm"
+                  tag="label"
+                  className={classNames("btn-simple", {
+                    active: bigChartData === "data3",
+                  })}
+                  onClick={() => setBgChartData("data3")}
+                >
+                  <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                    Machines
+                        </span>
+                  <span className="d-block d-sm-none">
+                    <i className="tim-icons icon-tap-02" />
+                  </span>
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Card>
+        </Row>
+        <Row>
           <Col xs="12">
             <Card>
               <CardHeader>
@@ -71,41 +133,42 @@ function Dashboard(props) {
                     {/* TODO: fix title alignment */}
                     <CardTitle tag="h1">Factory 1</CardTitle>
                   </Col>
-                  <Col sm="2">{/* Space holder for more data */}</Col>
+                  
                   {/* TODO: load data dynamically into these values */}
-                  <Col sm="1">
-                    <div className="text-center">
-                      <CardTitle tag="h3">100</CardTitle>
+                  <Col sm="3">
+                    <div className="text-right">
+                      <CardTitle tag="h3">3h 36min</CardTitle>
                       <h5 className="card-category">Last Active</h5>
                     </div>
                   </Col>
+                  
                   <Col sm="1">
                     <div className="text-center">
-                      <CardTitle tag="h3">100</CardTitle>
+                      <CardTitle tag="h3">80</CardTitle>
                       <h5 className="card-category">On Time Delivery</h5>
                     </div>
                   </Col>
                   <Col sm="1">
                     <div className="text-center">
-                      <CardTitle tag="h3">100</CardTitle>
+                      <CardTitle tag="h3">274</CardTitle>
                       <h5 className="card-category">Completed Jobs</h5>
                     </div>
                   </Col>
                   <Col sm="1">
                     <div className="text-center">
-                      <CardTitle tag="h3">100</CardTitle>
+                      <CardTitle tag="h3">126</CardTitle>
                       <h5 className="card-category">Pending Jobs</h5>
                     </div>
                   </Col>
                   <Col sm="1">
                     <div className="text-center">
-                      <CardTitle tag="h3">100</CardTitle>
+                      <CardTitle tag="h3">43</CardTitle>
                       <h5 className="card-category">Scrapped Jobs</h5>
                     </div>
                   </Col>
                   <Col sm="1">
                     <div className="text-center">
-                      <CardTitle tag="h3">100</CardTitle>
+                      <CardTitle tag="h3">57</CardTitle>
                       <h5 className="card-category">Rejected Jobs</h5>
                     </div>
                   </Col>
@@ -126,7 +189,10 @@ function Dashboard(props) {
               <CardHeader>
                 <Row>
                   <Col className="text-left" sm="8">
-                    <CardTitle tag="h3">Use Time</CardTitle>
+                    <h5 className="card-category">Use Time</h5>
+                    <CardTitle tag="h3">
+                      <i className="tim-icons icon-time-alarm text-info" /> 14h 24min
+                </CardTitle>
                   </Col>
                 </Row>
               </CardHeader>
@@ -150,7 +216,10 @@ function Dashboard(props) {
               <CardHeader>
                 <Row>
                   <Col className="text-left" sm="8">
-                    <CardTitle tag="h3">Idle Time</CardTitle>
+                  <h5 className="card-category">Idle Time</h5>
+                    <CardTitle tag="h3">
+                      <i className="tim-icons icon-time-alarm text-warning" /> 6h 0min
+                </CardTitle>
                   </Col>
                 </Row>
               </CardHeader>
@@ -174,7 +243,10 @@ function Dashboard(props) {
               <CardHeader>
                 <Row>
                   <Col className="text-left" sm="8">
-                    <CardTitle tag="h3">Downtime</CardTitle>
+                  <h5 className="card-category">Downtime</h5>
+                    <CardTitle tag="h3">
+                      <i className="tim-icons icon-time-alarm text-danger" /> 3h 36min
+                </CardTitle>
                   </Col>
                 </Row>
               </CardHeader>
@@ -198,7 +270,10 @@ function Dashboard(props) {
               <CardHeader>
                 <Row>
                   <Col className="text-left" sm="8">
-                    <CardTitle tag="h3">On Time Delivery</CardTitle>
+                  <h5 className="card-category">On Time Delivery</h5>
+                    <CardTitle tag="h3">
+                      <i className="tim-icons icon-send text-success" /> 80
+                </CardTitle>
                   </Col>
                 </Row>
               </CardHeader>
@@ -222,11 +297,14 @@ function Dashboard(props) {
           <Col lg="6">
             <Card className="card-chart">
               <CardHeader>
-                <CardTitle tag="h3">Job Completion Rate</CardTitle>
+              <h5 className="card-category">Job Completion Rate</h5>
+                    <CardTitle tag="h3">
+                      <i className="tim-icons icon-check-2 text-success" /> Avg 125/day
+                </CardTitle>
               </CardHeader>
               <CardBody>
                 <div className="chart-area large">
-                <BarChart
+                  <BarChart
                     labels={["M", "T", "W", "T", "F", "S", "S"]}
                     data={[80, 178, 123, 30, 50, 150, 265]}
                     color={"rgba(29,248,179,0.2)"}
@@ -239,11 +317,14 @@ function Dashboard(props) {
           <Col lg="6" md="12">
             <Card className="card-chart">
               <CardHeader>
-                <CardTitle tag="h3">All Jobs</CardTitle>
+              <h5 className="card-category">All Jobs</h5>
+                    <CardTitle tag="h3">
+                      <i className="tim-icons icon-bullet-list-67 text-info" /> 500
+                </CardTitle>
               </CardHeader>
               <CardBody>
                 <div className="chart-area large">
-                  <PieChart data={[274, 126, 43, 57]}/>
+                  <PieChart data={[274, 126, 43, 57]} />
                 </div>
               </CardBody>
             </Card>
