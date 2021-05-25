b6d9bc26030bfafc20e923b3b597b097e3806aa4
Mamta <mamtajhamat27@gmail.com>
2021-05-25 11:55:32 +0300

    datepicker added

--- src/components/BootstrapModal.jsx
+++ src/components/BootstrapModal.jsx
@@ -2,52 +2,48 @@ import { Button, Modal } from "react-bootstrap";
 import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { FormGroup, Input } from "reactstrap";
-import axios from 'axios';
-
+import { FormGroup, Form, Input, Label } from "reactstrap";
+import axios from "axios";
 
 class BootstrapModal extends React.Component {
-  
   constructor() {
     super();
     this.state = {
       showHide: false,
       modalIsOpen: true,
-      startDate: 1,
-      setStartDate: 1,
+      startDate: new Date(),
+      selectNewDate : new Date(),
       roles: [],
-      firstName:'',
-      lastName:'',
-      city:'',
-      company: '',
-      role: ''
+      firstName: "",
+      lastName: "",
+      city: "",
+      company: "",
+      role: "",
     };
     this.getRoles = this.getRoles.bind(this);
-    this.componentDidMount=this.componentDidMount.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
     this.changeDate = this.changeDate.bind(this);
-    this.onChange=this.onChange.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
-  onChange(e){
+  onChange(e) {
     e.preventDefault();
-    let newState={...this.state};
-    newState[e.target.name]=e.target.value;
+    let newState = { ...this.state };
+    newState[e.target.name] = e.target.value;
     this.setState(newState);
-}
+  }
 
-  async componentDidMount(){
+  async componentDidMount() {
     await this.getRoles();
-   
-}
+  }
 
   handleModalShowHide() {
     this.setState({ showHide: !this.state.showHide });
-  };
-
-  changeDate () {
-    this.setState({ startDate : this.setState(new Date())});
   }
 
+  changeDate(date) {
+    this.setState({ startDate: date });
+  }
 
   toggleModal = () => {
     this.setState((prevState) => ({
@@ -55,78 +51,109 @@ class BootstrapModal extends React.Component {
     }));
   };
 
-  getRoles = async ()=>{
-
-    axios.get('http://localhost:7100/smart-factory/roles').then(response => {
+  getRoles = async () => {
+    axios
+      .get("http://localhost:7100/smart-factory/roles")
+      .then((response) => {
         this.setState({
           ...this.state,
           roles: response.data.roles,
         });
 
         console.log(this.state);
-
-        
-      }).catch(error => {
-        alert('Could not connect to Server. Make sure Mockoon server is on if you are using it')
-        
-        });
-};
+      })
+      .catch((error) => {
+        alert(
+          "Could not connect to Server. Make sure Mockoon server is on if you are using it"
+        );
+      });
+  };
 
   render() {
-    // const [startDate, setStartDate] = this.setState(new Date());
-  
     return (
       <div>
         <Button variant="primary" onClick={() => this.handleModalShowHide()}>
           Create New User
         </Button>
 
-        <Modal show={this.state.showHide} className="modal">
+        <Modal  show={this.state.showHide} className="modal">
           <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
             <Modal.Title>New User</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <FormGroup >
-              <label>First Name</label>
-              <Input type="text" name="firstName" value={this.state.firstName} onChange={this.onChange}/>
-            </FormGroup>
-            <FormGroup>
-              <label>Last Name</label>
-              <Input type="text" name="lastName" value={this.state.lastName} onChange={this.onChange}/>
-            </FormGroup>
-            <FormGroup>
-              <label>Company Name</label>
-              <Input type="text" name="company" value={this.state.company} onChange={this.onChange}/>
-            </FormGroup>
+            <Form>
+              <FormGroup>
+                <Label for="firstName">First Name</Label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="firstName"
+                  name="firstName"
+                  value={this.state.firstName}
+                  onChange={this.onChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="lastName">Last Name</Label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.onChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="companyName">Company Name</Label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="companyName"
+                  name="company"
+                  value={this.state.company}
+                  onChange={this.onChange}
+                />
+              </FormGroup>
 
-
-            <FormGroup style={{ width: "100%" }}>
-              <label className="label-fix">Role</label>
-              <br />
-              <select
-                className="select-padding"
-                aria-label="Default select example"
-              >
-                <option selected>Select Role</option>
-                { this.state.roles.map((role => <option key = {role.roleID} value={role.roleID}> {role.name} </option>))}
-                {/* <option value="1">Company Admin</option>
-                <option value="2">Manufacturing Operations Manager</option>
-                <option value="3">Factory Manager</option>
-                <option value="4">Maintenance Technician</option>
-                <option value="5">Device Operator</option>
-                <option value="6">Factory Operator</option> */}
-              </select>
-            </FormGroup>
-
+              <FormGroup>
+                <Label for="select" className="label-fix">
+                  Role
+                </Label>
+                <br />
+                <Input
+                  type="select"
+                  name="select"
+                  id="select"
+                  className="select-padding"
+                  aria-label="Default select example"
+                >
+            
+                  <option selected>Select Role</option>
+                  {this.state.roles.map((role) => (
+                    <option key={role.roleID} value={role.roleID}>
+                      {" "}
+                      {role.name}{" "}
+                    </option>
+                  ))}
                
-            <FormGroup style={{ width: "100%" }}>
-              <label>Deadline</label>
-              <br></br>
-              {/* <DatePicker selected={startDate} onChange={date => setStartDate(date)} /> */}
-              {/* <Input  onClick={() => this.showCalender()} /> */}
-              <DatePicker className="date-fix" onClick={() => this.changeDate()}/>
-            </FormGroup>
+                </Input>
+              </FormGroup>
+
+              <FormGroup style={{ width: "100%" }}>
+                <label>Deadline</label>
+                <br></br>
+
+                <DatePicker
+                  className="date-fix"
+                  selected={this.state.startDate}
+                  dateFormat="dd/MM/yyyy"
+                  onChange={this.changeDate}
+                />
+              </FormGroup>
+            </Form>
           </Modal.Body>
+
 
 
           <Modal.Footer>
